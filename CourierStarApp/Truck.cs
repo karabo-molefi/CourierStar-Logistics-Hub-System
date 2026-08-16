59ad7eed18c49d8cb5200fb0ce9273f9ba58bb70
Karabo Molefi <karabomolefi@icloud.com>
2026-08-16 16:42:52 +0200

    add truck class

---
+++ CourierStarApp/Truck.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourierStarApp
+{
+    internal class Truck: Vehicle
+    {
+        private double MaxWeight = 4500;//kilograms
+        private double MaxDimension = 30;//cubic meters
+
+        // Constructor
+        Truck(int VehicleId, double CurrentLoad, double MaxCapacity, double MaxWeight, double MaxDimension) : base(VehicleId, CurrentLoad, MaxCapacity)
+        {
+            this.MaxWeight = MaxWeight;
+            this.MaxDimension = MaxDimension;
+        }
+
+        //Methods 
+        public override double CalculateRemainingCapacity()
+        {
+            return MaxCapacity - CurrentLoad;
+        }
+    }
+}
