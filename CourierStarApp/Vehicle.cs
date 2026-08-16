a8ea0e212ae952a4cb13a8b6834226a8a2473197
Karabo Molefi <karabomolefi@icloud.com>
2026-08-16 14:43:50 +0200

    add members to vehicles class

--- CourierStarApp/Vehicle.cs
+++ CourierStarApp/Vehicle.cs
@@ -8,5 +8,48 @@ namespace CourierStarApp
 {
     internal class Vehicle
     {
+        protected int VehicleId;
+        protected double CurrentLoad;
+        protected double MaxCapacity;
+
+        // Constructor 
+        public Vehicle (int VehicleId, double CurrentLoad, double MaxCapacity)
+        {
+            this.VehicleId = VehicleId;
+            this.CurrentLoad = CurrentLoad;
+            this.MaxCapacity = MaxCapacity;
+        } 
+
+        // Methods
+
+        public void LoadPackage(/*Package rp*/)
+        {
+
+        }
+
+        public virtual double CalculateRemainingCapacity()
+        {
+            double RemainingCapacity = MaxCapacity - CurrentLoad;
+            return RemainingCapacity;
+        }
+
+        public void TriggerWarning()
+        {
+            // implement warning
+        }
+
+        public void PrintDetails()
+        {
+            Console.WriteLine($"VehicleId: {VehicleId}\n" +
+                $"CurrentLoad: {CurrentLoad}\n" +
+                $"MaxCapacity: {MaxCapacity}" +
+                $"Vehicle Type: ");// add type of vehicle
+
+        }
+
+
+
+
+        
     }
 }
