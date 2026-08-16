568a506c1c77ac6029565f146f3b528d48093fd7
Karabo Molefi <karabomolefi@icloud.com>
2026-08-16 22:30:56 +0200

    create ISearch Interface

--- CourierStarApp/VehicleRepository.cs
+++ CourierStarApp/VehicleRepository.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Linq;
 
 namespace CourierStarApp
 {
@@ -15,7 +14,7 @@ namespace CourierStarApp
         Search
     }
 
-    internal class VehicleRepository: IPrintable
+    internal class VehicleRepository : IPrintable, ISearch
     {
         // menu for managing vehicles
 
